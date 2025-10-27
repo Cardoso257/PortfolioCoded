44d7557eb91d81e1ee8c9900236ebdee3a515b87
Cardoso257 <22401792@aluno.cotemig.com.br>
2025-10-27 08:52:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+// MODO CLARO/ESCURO COM MEMÓRIA ENTRE PÁGINAS
+let botaoModo = document.getElementById('modo-btn');
+
+let modoSalvo = localStorage.getItem('modoTema');
+
+if (modoSalvo === 'claro') {
+    document.body.classList.add('claro');
+}
+
+botaoModo.addEventListener('click', () => {
+    document.body.classList.toggle('claro');
+
+    if (document.body.classList.contains('claro')) {
+        localStorage.setItem('modoTema', 'claro');
+    } else {
+        localStorage.setItem('modoTema', 'escuro');
+    }
+});
+
+// ---------- MODAL DE CONTATO ----------
+const abrirModal = document.getElementById('abrirModal');
+const modal = document.getElementById('modal');
+const fecharModal = document.getElementById('fecharModal');
+const formContato = document.getElementById('formContato');
+const msgEnviada = document.getElementById('msgEnviada');
+
+if (abrirModal) {
+  abrirModal.addEventListener('click', () => {
+    modal.style.display = 'flex';
+  });
+}
+
+if (fecharModal) {
+  fecharModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+    msgEnviada.textContent = '';
+    formContato.reset();
+  });
+}
+
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
+
+if (formContato) {
+  formContato.addEventListener('submit', (e) => {
+    e.preventDefault();
+    msgEnviada.textContent = '✅ Mensagem enviada com sucesso!';
+    formContato.reset();
+
+    setTimeout(() => {
+      modal.style.display = 'none';
+      msgEnviada.textContent = '';
+    }, 2000);
+  });
+}
